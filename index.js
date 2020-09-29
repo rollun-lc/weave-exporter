ec9efc05e2c16da75627cc20f1b8cb7d9841985d
max_tricky_lake <mdovhopo@student.unit.ua>
2020-09-29 14:22:28 +0300

    refactor exporter, to crush on errors

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const {exec, execSync} = require('child_process');
+const {execSync} = require('child_process');
 const axios = require('axios');
 
-exec(`weave dns-lookup ${process.env.PROP_PUSHGATEWAY_HOSTNAME || 'prom-pushgateway'}`, (error, stdout, stderr) => {
-    console.log('prom-pushgateway ip:', stdout);
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-    }
-    const prom_pushgateway_ip = stdout.trim() + ':9091';
-    const interval = +(process.env.WEAVE_SCRAPE_INTERVAL || '15');
-    const instance = `${(process.env.INSTANCE_NAME || execSync('hostname'))}`.trim();
-    console.log(`starting exporter: proppush: ${prom_pushgateway_ip}, hostname: ${instance}, interval: ${interval}, metrics host: ${process.env.WEAVE_METRICS_HOST}`);
-    // scrape every 10s
-    setInterval(() => {
-      (async () => {
-        try {
-          console.log(new Date().toISOString(), 'request weave metrics...');
-          const {data: metrics} = await axios.get(`http://${process.env.WEAVE_METRICS_HOST || 'localhost:6782'}/metrics`, {timeout: 5000});
-          console.log(new Date().toISOString(), 'got metrics');
-          const {data: prom_response} = await axios.post('http://' + prom_pushgateway_ip + '/metrics/job/weave_exporter/instance/' + instance, metrics, {timeout: 5000});
-          console.log(new Date().toISOString(), 'got prom response', prom_response);
-        } catch (err) {
-          console.log('Scraping error', err);
-        }
-      })();
-    }, interval * 1000);
-  });
+const prom_pushgateway_ip = execSync(`weave dns-lookup ${process.env.PROM_PUSHGATEWAY_HOSTNAME || 'prom-pushgateway'}`).trim() + ':' + (process.env.PROM_PUSHGATEWAY_PORT || '9091');
+const interval = +(process.env.WEAVE_SCRAPE_INTERVAL || '15');
+const instance = `${(process.env.INSTANCE_NAME || execSync('hostname'))}`.trim();
 
-//console.log('out', out);
+console.log(`Starting exporter with: push_gateway_ip: ${prom_pushgateway_ip}, hostname: ${instance}, interval: ${interval}, metrics host: ${process.env.WEAVE_METRICS_HOST}`);
+
+setInterval(() => {
+  (async () => {
+    try {
+      console.log(new Date().toISOString(), 'requesting weave metrics...');
+      const {data: metrics} = await axios.get(`http://${process.env.WEAVE_METRICS_HOST || 'localhost:6782'}/metrics`, {timeout: 5000});
+      console.log(new Date().toISOString(), 'got metrics, sending to prometheus push gateway');
+      await axios.post('http://' + prom_pushgateway_ip + '/metrics/job/weave_exporter/instance/' + instance, metrics, {timeout: 5000});
+      console.log(new Date().toISOString(), 'metrics delivered to prometheus push gateway');
+    } catch (err) {
+      console.log('Scraping error', err.message);
+      process.exit(1);
+    }
+  })();
+}, interval * 1000);
